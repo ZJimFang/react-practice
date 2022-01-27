c91303e179b7bf1f37fd9201b2a8b2fff7c6346f
Jim Fang <jmjmjim1101@gmail.com>
2022-01-27 12:07:50 +0800

    0127

--- webpact-tutorial/src/index.js
+++ webpact-tutorial/src/index.js
@@ -1,1 +1,16 @@
-console.log('hello')
+import './index.css'
+
+const btn = document.getElementById('btn');
+const div = document.getElementById('num');
+
+btn.addEventListener('click', function () {
+    let a = parseInt(num.innerText, 10);
+    num.innerText = a + 1
+})
+
+class Test {
+    #a = 1
+}
+
+const t1 = new Test();
+console.log(t1.a)
--- webpact-tutorial/webpack.config.js
+++ webpact-tutorial/webpack.config.js
@@ -1,16 +1,40 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-const webpack = require('webpack'); //to access built-in plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js',
+    filename: 'main.[hash].bundle.js'
   },
   module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.gif/,
+        type: 'asset/resource'
+      }
+    ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
-  mode: 'production',
+  devtool: 'source-map',
+  plugins: [
+    new CleanWebpackPlugin({
+      verbose: true,
+    }),
+    new HtmlWebpackPlugin({ template: './base.html' }),
+    new MiniCssExtractPlugin({ filename: 'main.[hash].css' }),
+  ]
 };
